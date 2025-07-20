fc61fe1061c8138751c0354797b868eac21c8315
sherry-90 <shahrzad.imani74@gmail.com>
2025-07-20 16:47:22 +0330

    Add header and movie search component with styling and assets
    - Implemented responsive site header with logo
    - Created reusable movie search component with input and icon
    - Applied custom styling using Tailwind CSS
    - Added icons and images for logo, background and search UI

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,20 @@
-import React from 'react'
+import React, { useState } from "react";
+import Search from "./components/Search";
 
 export default function App() {
+  const [searchTerm, setSearchTerm] = useState('');
+
   return (
-    <div className="text-3xl font-bold underline ">Hello World!</div>
-  )
+    <main>
+      <div className="pattern">
+        <div className="wrapper">
+          <header>
+            <img src="./hero.png" alt="Hero Banner"/>
+            <h1>Find <span className="text-gradient">Movies</span> You'll enjoy witout the Hassle</h1>
+          </header>
+          <Search searchTerm={searchTerm} setSearchTerm={setSearchTerm}/>
+        </div>
+      </div>
+    </main>
+  );
 }
